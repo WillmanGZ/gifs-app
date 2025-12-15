cffc84d67bf57975d52b2348e0ed4d740335c235
WillmanGZ <willmang@uninorte.edu.co>
2025-12-14 20:05:40 -0500

    feat: breake app down in components

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,42 +1,26 @@
+import { GifList } from "./gifs/GifList";
+import { PreviousSearches } from "./gifs/PreviousSearches";
 import { mockGifs } from "./mock-data/gifs.mock";
+import { CustomHeader } from "./shared/components/CustomHeader";
+import { SharedBar } from "./shared/components/SharedBar";
 
 export const GifsApp = () => {
   return (
     <>
       {/* Header */}
-      <div className="content-center">
-        <h1>Buscador de Gifs</h1>
-        <p>Descubre y comparte el gif perfecto</p>
-      </div>
+      <CustomHeader
+        title="Buscador de Gifs"
+        description="Descubre y comparte el Gif perfecto"
+      />
 
       {/* Search */}
-      <div className="search-container">
-        <input type="text" placeholder="Buscar gifs" />
-        <button>Buscar</button>
-      </div>
+      <SharedBar placeholder="Buscar gifs" />
 
       {/* Previous searches */}
-      <div className="previous-searches">
-        <h2>Busquedas previas</h2>
-        <ul className="previous-searches-list">
-          <li>Goku</li>
-          <li>Saitama</li>
-          <li>Elden Ring</li>
-        </ul>
-      </div>
+      <PreviousSearches searches={["Goku", "Dragon Ball"]} />
 
       {/* Gifs */}
-      <div className="gifs-container">
-        {mockGifs.map((gif) => (
-          <div key={gif.id} className="gif-card">
-            <img src={gif.url} alt={gif.title} />
-            <h3>{gif.title}</h3>
-            <p>
-              {gif.width}x{gif.height}(1.5mb)
-            </p>
-          </div>
-        ))}
-      </div>
+      <GifList gifs={mockGifs} />
     </>
   );
 };
