9249d842a6c3b3b76ed0fdde381a7a5ddb173747
WillmanGZ <willmang@uninorte.edu.co>
2025-12-14 22:22:33 -0500

    feat: integrate Giphy API for GIF search functionality and update components

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,22 +1,25 @@
 import { useState } from "react";
 import { GifList } from "./gifs/GifList";
 import { PreviousSearches } from "./gifs/PreviousSearches";
-import { mockGifs } from "./mock-data/gifs.mock";
 import { CustomHeader } from "./shared/components/CustomHeader";
 import { SharedBar } from "./shared/components/SharedBar";
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action";
+import type { Gif } from "./gifs/interfaces/gif.interface";
 
 export const GifsApp = () => {
+  const [gifs, setGifs] = useState<Gif[]>([]);
   const [previousSearches, setPreviousSearches] = useState<string[]>([]);
 
-  const handleSearchClicked = (term: string) => {
-    console.log({ term });
-  };
-
-  const handleSearch = (query: string) => {
+  const handleSearch = async (query: string) => {
     if (previousSearches.includes(query)) return;
 
     const newHistory = [query, ...previousSearches];
     setPreviousSearches(newHistory.slice(0, 8));
+    setGifs(await getGifsByQuery(query));
+  };
+
+  const handleSearchClicked = (query: string) => {
+    handleSearch(query);
   };
 
   return (
@@ -37,7 +40,7 @@ export const GifsApp = () => {
       />
 
       {/* Gifs */}
-      <GifList gifs={mockGifs} />
+      <GifList gifs={gifs} />
     </>
   );
 };
