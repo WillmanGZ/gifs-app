f66277275fe170cf5c4d1f55d6ef127f3e66cc44
WillmanGZ <willmang@uninorte.edu.co>
2025-12-14 21:37:54 -0500

    feat: implement search functionality and update component interactions

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { GifList } from "./gifs/GifList";
 import { PreviousSearches } from "./gifs/PreviousSearches";
 import { mockGifs } from "./mock-data/gifs.mock";
@@ -5,6 +6,19 @@ import { CustomHeader } from "./shared/components/CustomHeader";
 import { SharedBar } from "./shared/components/SharedBar";
 
 export const GifsApp = () => {
+  const [previousSearches, setPreviousSearches] = useState<string[]>([]);
+
+  const handleSearchClicked = (term: string) => {
+    console.log({ term });
+  };
+
+  const handleSearch = (query: string) => {
+    if (previousSearches.includes(query)) return;
+
+    const newHistory = [query, ...previousSearches];
+    setPreviousSearches(newHistory.slice(0, 8));
+  };
+
   return (
     <>
       {/* Header */}
@@ -14,10 +28,13 @@ export const GifsApp = () => {
       />
 
       {/* Search */}
-      <SharedBar placeholder="Buscar gifs" />
+      <SharedBar placeholder="Buscar gifs" onQuery={handleSearch} />
 
       {/* Previous searches */}
-      <PreviousSearches searches={["Goku", "Dragon Ball"]} />
+      <PreviousSearches
+        searches={previousSearches}
+        onLabelClicked={handleSearchClicked}
+      />
 
       {/* Gifs */}
       <GifList gifs={mockGifs} />
--- src/shared/components/SharedBar.tsx
+++ src/shared/components/SharedBar.tsx
@@ -1,12 +1,45 @@
+import { useCallback, useEffect, useState, type KeyboardEvent } from "react";
 interface Props {
   placeholder?: string;
+  onQuery: (query: string) => void;
 }
 
-export const SharedBar = ({ placeholder = "Buscar" }: Props) => {
+export const SharedBar = ({ placeholder = "Buscar", onQuery }: Props) => {
+  const [query, setQuery] = useState("");
+
+  const handleSearch = useCallback(() => {
+    if (query.trim() == "") return;
+
+    onQuery(query.trim().toLowerCase());
+    setQuery("");
+  }, [query, onQuery]);
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      handleSearch();
+    }, 700);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [query, handleSearch]);
+
   return (
     <div className="search-container">
-      <input type="text" placeholder={placeholder} />
-      <button>Buscar</button>
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={handleSearch}>Buscar</button>
     </div>
   );
 };
